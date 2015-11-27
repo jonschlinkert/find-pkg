a1f22f644a4d8cbeca7d6f40eb73e1534198d173
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 14:43:05 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * find-pkg <https://github.com/jonschlinkert/find-pkg>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
