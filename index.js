d9ac63c36762313d2fb7a72e678725a8fc946dcb
jonschlinkert <jon.schlinkert@sellside.com>
2018-03-28 01:58:57 -0400

    use latest find-file-up

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
-/*!
- * find-pkg <https://github.com/jonschlinkert/find-pkg>
- *
- * Copyright (c) 2015-2017, Jon Schlinkert.
- * Released under the MIT License.
- */
-
 'use strict';
 
-/**
- * Module dependencies
- */
+const find = require('find-file-up');
 
-var findFile = require('find-file-up');
-
-/**
- * Find package.json, starting with the given directory
- */
-
-module.exports = function(dir, limit, cb) {
-  return findFile('package.json', dir, limit, cb);
-};
-
-/**
- * Sync
- */
-
-module.exports.sync = function(dir, limit) {
-  return findFile.sync('package.json', dir, limit);
-};
+module.exports = find.bind(null, 'package.json');
+module.exports.sync = find.sync.bind(null, 'package.json');
--- test.js
+++ test.js
@@ -1,106 +1,118 @@
-/*!
- * find-pkg <https://github.com/jonschlinkert/find-pkg>
- *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 require('mocha');
-var fs = require('fs');
-var path = require('path');
-var resolve = require('resolve-dir');
-var assert = require('assert');
-var findPkg = require('./');
-var del = require('delete');
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+const del = require('delete');
+const resolve = require('resolve-dir');
+const find = require('./');
 
-describe('find-file', function() {
-  it('should resolve a file in the cwd:', function(cb) {
-    findPkg('.', function(err, fp) {
-      assert.equal(fp, path.resolve('package.json'));
-      cb();
+describe('find-pkg', function() {
+  describe('async', function() {
+    it('should work with promises', async() => {
+      const file = await find('.');
+      assert.equal(file, path.resolve('package.json'));
     });
-  });
 
-  it('should resolve a file from user home:', function(cb) {
-    fs.writeFile(resolve('~/package.json'), 'tmp', function(err) {
-      if (err) return cb(err);
-
-      findPkg('~/_a/b/c', function(err, fp) {
-        assert(fp === resolve('~/package.json'));
-        del(fp, {force: true}, cb);
-      });
+    it('should work with promises and default cwd', async() => {
+      const file = await find();
+      assert.equal(file, path.resolve('package.json'));
     });
-  });
 
-  it('should return undefined when the file does not exist:', function(cb) {
-    findPkg(resolve('~/a/b/c'), function(err, fp) {
-      assert(fp === undefined);
-      cb();
-    });
-  });
-
-  it('should resolve a file from one directory up:', function(cb) {
-    findPkg('test', function(err, fp) {
-      assert.equal(fp, path.resolve('package.json'));
-      cb();
-    });
-  });
-
-  it('should resolve package.json from multiple directories up:', function(cb) {
-    findPkg('test/a/b/c/c/', function(err, fp) {
-      assert.equal(fp, path.resolve('package.json'));
-      cb();
-    });
-  });
-
-  it('should stop at the given limit', function(cb) {
-    findPkg('fixtures/a/b/c/d/e', function(err, fp) {
-      assert.equal(fp, path.resolve('fixtures/a/b/package.json'));
-
-      findPkg('fixtures/a/b/c/d/e', 2, function(err, fp) {
-        assert.equal(fp, undefined);
+    it('should resolve a file in the cwd:', function(cb) {
+      find('.', function(err, fp) {
+        if (err) return cb(err);
+        assert.equal(fp, path.resolve('package.json'));
         cb();
       });
     });
+
+    it('should resolve a file from user home:', function(cb) {
+      fs.writeFile(resolve('~/package.json'), 'tmp', function(err) {
+        if (err) return cb(err);
+
+        find('~/_a/b/c', function(err, fp) {
+          if (err) return cb(err);
+          assert.equal(fp, resolve('~/package.json'));
+          del(fp, {force: true}, cb);
+        });
+      });
+    });
+
+    it('should return undefined when the file does not exist:', function(cb) {
+      find(resolve('~/a/b/c'), function(err, fp) {
+        if (err) return cb(err);
+        assert(!fp);
+        cb();
+      });
+    });
+
+    it('should resolve a file from one directory up:', function(cb) {
+      find('test', function(err, fp) {
+        if (err) return cb(err);
+        assert.equal(fp, path.resolve('package.json'));
+        cb();
+      });
+    });
+
+    it('should resolve package.json from multiple directories up:', function(cb) {
+      find('test/a/b/c/c/', function(err, fp) {
+        if (err) return cb(err);
+        assert.equal(fp, path.resolve('package.json'));
+        cb();
+      });
+    });
+
+    it('should stop at the given limit', function(cb) {
+      find('fixtures/a/b/c/d/e', function(err, fp) {
+        if (err) return cb(err);
+        assert.equal(fp, path.resolve('fixtures/a/b/package.json'));
+
+        find('fixtures/a/b/c/d/e', 2, function(err, fp) {
+          if (err) return cb(err);
+          assert(!fp);
+          cb();
+        });
+      });
+    });
   });
-});
 
-describe('find-file-sync', function() {
-  it('should resolve a file in the cwd:', function() {
-    var fp = findPkg.sync('.');
-    assert.equal(fp, path.resolve('package.json'));
-  });
+  describe('sync', function() {
+    it('should resolve a file in the cwd:', function() {
+      const fp = find.sync('.');
+      assert.equal(fp, path.resolve('package.json'));
+    });
 
-  it('should resolve a file from user home:', function() {
-    fs.writeFileSync(resolve('~/package.json'), 'tmp');
+    it('should resolve a file from user home:', function() {
+      fs.writeFileSync(resolve('~/package.json'), 'tmp');
 
-    var fp = findPkg.sync('~/_a/b/c');
-    assert(fp === resolve('~/package.json'));
-    del.sync(fp, {force: true});
-  });
+      const fp = find.sync('~/_a/b/c');
+      assert.equal(fp, resolve('~/package.json'));
+      del.sync(fp, {force: true});
+    });
 
-  it('should return undefined when the file does not exist:', function() {
-    var fp = findPkg.sync(resolve('~/a/b/c'));
-    assert(fp === undefined);
-  });
+    it('should return undefined when the file does not exist:', function() {
+      const fp = find.sync(resolve('~/a/b/c'));
+      assert(!fp);
+    });
 
-  it('should resolve a file from one directory up:', function() {
-    var fp = findPkg.sync('test');
-    assert.equal(fp, path.resolve('package.json'));
-  });
+    it('should resolve a file from one directory up:', function() {
+      const fp = find.sync('test');
+      assert.equal(fp, path.resolve('package.json'));
+    });
 
-  it('should stop at the given limit', function() {
-    var fp = findPkg.sync('fixtures/a/b/c/d/e');
-    assert.equal(fp, path.resolve('fixtures/a/b/package.json'));
+    it('should stop at the given limit', function() {
+      let fp = find.sync('fixtures/a/b/c/d/e');
+      assert.equal(fp, path.resolve('fixtures/a/b/package.json'));
 
-    fp = findPkg.sync('fixtures/a/b/c/d/e', 2);
-    assert.equal(fp, undefined);
-  });
+      fp = find.sync('fixtures/a/b/c/d/e', 2);
+      assert(!fp);
+    });
 
-  it('should resolve package.json from multiple directories up:', function() {
-    var fp = findPkg.sync('test/a/b/c/c/');
-    assert.equal(fp, path.resolve('package.json'));
+    it('should resolve package.json from multiple directories up:', function() {
+      const fp = find.sync('test/a/b/c/c/');
+      assert.equal(fp, path.resolve('package.json'));
+    });
   });
 });
